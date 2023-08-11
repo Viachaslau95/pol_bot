b0e7a98b413df0c9ac2beacfb11363cfdb8d1d88
V.Kulchynski <6891094@mail.ru>
2023-08-11 18:17:22 +0300

    added new fields for clients model

--- core/admin.py
+++ core/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
 
-# Register your models here.
+from core.models import Client
+
+
+@admin.register(Client)
+class ClientAdmin(admin.ModelAdmin):
+    list_display = ('id', 'reg_email', 'reg_password')
--- core/models.py
+++ core/models.py
@@ -1,3 +1,21 @@
 from django.db import models
 
-# Create your models here.
+
+class Client(models.Model):
+    reg_email = models.CharField(max_length=120)
+    reg_password = models.CharField(max_length=120)
+
+    national_id = models.CharField(max_length=250)
+    firstname = models.CharField(max_length=100)
+    lastname = models.CharField(max_length=100)
+    gender = models.CharField(max_length=20, blank=True, null=True)
+    passport_number = models.CharField(max_length=100)
+    passport_expire_date = models.CharField(max_length=10)
+    code_country = models.IntegerField(default=0)
+    contact_number = models.IntegerField(default=0)
+    admin_email = models.CharField(max_length=120)
+
+    is_active = models.BooleanField(default=True)
+
+    def __str__(self):
+        return f'{self.firstname}-{self.lastname}'
