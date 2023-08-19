c5d93dc159fe1e365367f7f7aa6eb11aff516713
V.Kulchynski <6891094@mail.ru>
2023-08-19 15:04:21 +0300

    added new field in admin added new method for field(M2M)

--- core/admin.py
+++ core/admin.py
@@ -5,7 +5,13 @@ from core.models import Client, City
 
 @admin.register(Client)
 class ClientAdmin(admin.ModelAdmin):
-    list_display = ('id', 'firstname', 'is_active')
+    list_display = ('id', 'firstname', 'is_active', 'list_cities', 'visa_sub_category')
+    list_filter = ('cities',)
+
+    def list_cities(self, obj):
+        return ", ".join([related_obj.title for related_obj in obj.cities.all()])
+
+    list_cities.short_description = 'Cities'
 
 
 @admin.register(City)
