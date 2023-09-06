463f8c26a977309964d28264836f1221789a8f2d
V.Kulchynski <6891094@mail.ru>
2023-09-06 16:38:51 +0300

    added age processing(18<age<58,63) added new function for visa without msi

--- core/management/commands/pol_bot.py
+++ core/management/commands/pol_bot.py
@@ -1,5 +1,6 @@
 import threading
 import time
+from datetime import datetime
 from random import choice
 
 from django.core.management import BaseCommand
@@ -528,7 +529,19 @@ class Command(BaseCommand):
                 continue_button.click()
                 print("Button has been clicked")
                 self.should_break = False
-                self.your_detail(driver, client)
+                current_date = datetime.now()
+                birth_date = datetime.strptime(client.date_of_birth, "%d/%m/%Y")
+                age = current_date.year - birth_date.year
+                if (current_date.month, current_date.day) < (birth_date.month, birth_date.day):
+                    age -= 1
+                if age < 18:
+                    self.your_detail_without_msi(driver,client)
+                elif age > 58 and client.gender == "Female":
+                    self.your_detail_without_msi(driver, client)
+                elif age > 63 and client.gender == "Male":
+                    self.your_detail_without_msi(driver, client)
+                else:
+                    self.your_detail(driver, client)
         except Exception:
             self.should_break = False
             print(Exception)
@@ -580,63 +593,123 @@ class Command(BaseCommand):
             wait.until(EC.presence_of_element_located(button_locator))
             print("Кнопка 'Add another applicant' появилась")
 
-            self.some(driver)
+            self.find_slot(driver)
         except Exception as e:
             print("Произошла ошибка:", e)
 
         # driver.find_element(By.XPATH, "//button[contains(@class, 'mat-stroked-button') and text()=' Save ']").click()
 
-    # def some(self, driver):
-    #     print("in some")
-    #     while True:
-    #         try:
-    #             continue_button = driver.find_element(By.XPATH, "//button[contains(., 'Continue')]")
-    #             time.sleep(5)
-    #             continue_button.click()
-    #
-    #         except TimeoutException:
-    #             print("Кнопка 'Continue' не появилась за 60 минут")
-    #
-    #         WebDriverWait(driver, 600).until(
-    #             EC.presence_of_element_located((By.XPATH, "//h1[contains(text(), 'Book an Appointment')]")))
-    #         try:
-    #             table = driver.find_element(By.CLASS_NAME, "fc-scrollgrid-sync-table")
-    #             td_elements = table.find_elements(By.TAG_NAME, "td")
-    #             count_page = 0
-    #             desired_element_xpath = "//td[@class='fc-daygrid-day fc-day fc-day-fri fc-day-future date-availiable']"
-    #             while count_page < 2:
-    #                 for td_element in td_elements:
-    #                     if td_element.get_attribute("outerHTML") == driver.find_element(
-    #                             By.XPATH, desired_element_xpath).get_attribute("outerHTML"):
-    #                         time.sleep(2)
-    #                         td_element.click()
-    #                         time.sleep(3)
-    #
-    #                         try:
-    #                             WebDriverWait(driver, 60).until(
-    #                                 EC.presence_of_element_located(
-    #                                     (By.XPATH, "//h2[@class='fs-18 fs-sm-24 mt-40 mb-20 ng-star-inserted']")
-    #                                 )
-    #                             )
-    #                             time_element = driver.find_element(By.XPATH,
-    #                                                                "//tr[@class='hidden-item ng-star-inserted']")
-    #                             time_element.click()
-    #                             time.sleep(5)
-    #
-    #                             driver.find_element(By.XPATH,
-    #                                                 "//button[contains(@class, 'mat-raised-button') and contains(.//span, 'Continue')]").click()
-    #
-    #                         except Exception as e:
-    #                             print(e)
-    #
-    #                 driver.find_element(By.CLASS_NAME, "fc-next-button").click()
-    #                 count_page += 1
-    #
-    #             go_back_button = driver.find_element(By.XPATH,
-    #                                                  "//button[contains(@class, 'mat-stroked-button') and contains(.//span, 'Go Back')]")
-    #             go_back_button.click()
-    #             # ..........
-    #             # driver.find_element(By.CLASS_NAME, "fc-prev-button").click()
-    #         #
-    #         except TimeoutException:
-    #             print("Элемент <h1> с текстом 'Book an Appointment' не появился за 10 минут")
+    def your_detail_without_msi(self, driver, client):
+        WebDriverWait(driver, 120).until(
+            EC.presence_of_element_located((By.ID, "mat-input-2"))
+        )
+        driver.find_element(By.ID, "mat-input-2").send_keys(client.firstname)
+        time.sleep(3)
+
+        driver.find_element(By.ID, "mat-input-3").send_keys(client.lastname)
+        time.sleep(3)
+
+        driver.find_element(By.ID, "mat-select-8").click()
+        time.sleep(2)
+        if client.gender == 'Female':
+            gender_option = " Female "
+        elif client.gender == 'Male':
+            gender_option = " Male "
+        else:
+            gender_option = " Others / Transgender "
+
+        driver.find_element(By.XPATH,
+                            f"//span[contains(@class, 'mat-option-text') and text()='{gender_option}']").click()
+        time.sleep(3)
+
+        driver.find_element(By.ID, "mat-input-4").send_keys(client.passport_number)
+        time.sleep(3)
+
+        driver.find_element(By.ID, "passportExpirtyDate").send_keys(client.passport_expire_date)
+        time.sleep(3)
+
+        driver.find_element(By.ID, "mat-input-5").send_keys(client.code_country)
+        time.sleep(3)
+
+        driver.find_element(By.ID, "mat-input-6").send_keys(client.contact_number)
+        time.sleep(3)
+
+        driver.find_element(By.ID, "mat-input-7").send_keys(client.admin_email)
+        time.sleep(30)
+
+        # try:
+        #     button_save = driver.find_element(By.XPATH, "//span[contains(text(), 'Save')]")
+        #     button_save.click()
+        #     time.sleep(30)
+        #     wait = WebDriverWait(driver, 200)
+        #     element = wait.until(EC.visibility_of_element_located((By.XPATH, "//span[text()='Generate OTP']")))
+        #
+        #     self.some(driver)
+        # except Exception:
+        #     print("Error in 'your_detail_without_msi'!!! ")
+
+        try:
+            wait = WebDriverWait(driver, 3600)
+            button_locator = (By.XPATH, "//button[contains(., 'Add another applicant')]")
+            wait.until(EC.presence_of_element_located(button_locator))
+            print("Кнопка 'Add another applicant' появилась")
+
+            self.find_slot(driver)
+        except Exception as e:
+            print("Произошла ошибка:", e)
+
+    def find_slot(self, driver):
+        print("in find_slot")
+        # while True:
+        #     try:
+        #         continue_button = driver.find_element(By.XPATH, "//button[contains(., 'Continue')]")
+        #         time.sleep(5)
+        #         continue_button.click()
+        #
+        #     except TimeoutException:
+        #         print("Кнопка 'Continue' не появилась за 60 минут")
+        #
+        #     WebDriverWait(driver, 3600).until(
+        #         EC.presence_of_element_located((By.XPATH, "//h1[contains(text(), 'Book an Appointment')]")))
+        #     try:
+        #         table = driver.find_element(By.CLASS_NAME, "fc-scrollgrid-sync-table")
+        #         td_elements = table.find_elements(By.TAG_NAME, "td")
+        #         count_page = 0
+        #         desired_element_xpath = "//td[@class='fc-daygrid-day fc-day fc-day-fri fc-day-future date-availiable']"
+        #         while count_page < 2:
+        #             for td_element in td_elements:
+        #                 if td_element.get_attribute("outerHTML") == driver.find_element(
+        #                         By.XPATH, desired_element_xpath).get_attribute("outerHTML"):
+        #                     time.sleep(2)
+        #                     td_element.click()
+        #                     time.sleep(3)
+        #
+        #                     try:
+        #                         WebDriverWait(driver, 60).until(
+        #                             EC.presence_of_element_located(
+        #                                 (By.XPATH, "//h2[@class='fs-18 fs-sm-24 mt-40 mb-20 ng-star-inserted']")
+        #                             )
+        #                         )
+        #                         time_element = driver.find_element(By.XPATH,
+        #                                                            "//tr[@class='hidden-item ng-star-inserted']")
+        #                         time_element.click()
+        #                         time.sleep(5)
+        #
+        #                         driver.find_element(By.XPATH,
+        #                                             "//button[contains(@class, 'mat-raised-button') and contains(.//span, 'Continue')]").click()
+        #
+        #                     except Exception as e:
+        #                         print(e)
+        #
+        #
+        #             driver.find_element(By.CLASS_NAME, "fc-next-button").click()
+        #             count_page += 1
+        #
+        #         go_back_button = driver.find_element(By.XPATH,
+        #                                              "//button[contains(@class, 'mat-stroked-button') and contains(.//span, 'Go Back')]")
+        #         go_back_button.click()
+        #         # ..........
+        #         # driver.find_element(By.CLASS_NAME, "fc-prev-button").click()
+        #     #
+        #     except TimeoutException:
+        #         print("Элемент <h1> с текстом 'Book an Appointment' не появился за 60 минут")
